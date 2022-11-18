bc2399d85a79231602d90e20b803fc95f30f954d
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2022-11-18 13:57:50 -0500

    merge sandbox

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 
 package = 'slugify'
-python_requires = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*"
+python_requires = ">=3.7"
 here = os.path.abspath(os.path.dirname(__file__))
 
 install_requires = ['text-unidecode>=1.3']
@@ -75,11 +75,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     entry_points={'console_scripts': ['slugify=slugify.__main__:main']},
 )
